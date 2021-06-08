39a6405cd606add151eb9bb58780da48a5bb62bb
Joel Groomer <joelgroomer@gmail.com>
2021-06-07 19:23:11 -0700

    Initial Commit

---
+++ Rock Paper What/Rock_Paper_WhatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_WhatApp.swift
+//  Rock Paper What
+//
+//  Created by Joel Groomer on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_WhatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
